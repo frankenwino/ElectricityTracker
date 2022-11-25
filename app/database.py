57366640ef2821493264a0148b3007a44a7193f3
Andrew Browne <3808083+frankenwino@users.noreply.github.com>
2022-11-25 18:00:24 +0100

    Changed database file path to absolute path.

--- app/database.py
+++ app/database.py
@@ -1,9 +1,13 @@
 import sqlalchemy as db
+from datetime import datetime
+import pathlib
 
 def create_database():
     metadata.create_all(engine)
 
-engine = db.create_engine("sqlite:///electricity.db")
+db_file_path = pathlib.Path(__file__).parent.joinpath("electricity.db")
+# engine = db.create_engine("sqlite:///electricity.db")
+engine = db.create_engine(f"sqlite:///{db_file_path}")
 connection = engine.connect()
 metadata = db.MetaData()
 day_ahead = db.Table(
@@ -15,6 +19,27 @@ day_ahead = db.Table(
         db.Column("end_hour", db.Integer()),
         db.Column("sek_per_kwh", db.Float())
     )
+def current_date_start_hour():
+    now = datetime.now().strftime("%Y%m%d%H")
+    
+    return int(now)
 
-# if __name__ == "__main__":
-create_database()
+def latest_price_data():
+    query = db.select([day_ahead.columns.date, day_ahead.columns.start_hour, day_ahead.columns.end_hour, day_ahead.columns.sek_per_kwh]).where(day_ahead.columns.date_start_hour >= current_date_start_hour())
+    ResultProxy = connection.execute(query)
+    ResultSet = ResultProxy.fetchall()
+    
+    return ResultSet
+    # for row in ResultSet:
+    #     print(row.date, row.start_hour, row.end_hour, row.sek_per_kwh)
+
+
+if __name__ == "__main__":
+    # create_database()
+    # print(current_date_start_hour())
+    
+    # query = db.select([day_ahead.columns.date, day_ahead.columns.start_hour, day_ahead.columns.end_hour, day_ahead.columns.sek_per_kwh]).where(day_ahead.columns.date_start_hour >= current_date_start_hour())
+    # ResultProxy = connection.execute(query)
+    # ResultSet = ResultProxy.fetchall()
+    lpd = latest_price_data()
+    print(lpd[0].start_hour)
